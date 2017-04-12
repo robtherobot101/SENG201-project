acd5e5316a88b304eaca2b9af14e85ac425dab20
Noel Bisson <nbi21@uclive.ac.nz>
2017-04-12 15:43:55 +1200

    Add files via upload

---
+++ LeTestDeNoel.java
@@ -0,0 +1,13 @@
+package testClasses;
+
+public class LeTestDeNoel {
+	public void dreamWistfully() {
+		System.out.println("I am a cat, cat's like sleeping. \nCat's like sleeping and seem to have done fine for themselves as a species.");
+		System.out.println("Therefore I should sleep. \nNow I just need to find a similarly valid argument for playing Dota.");
+		}
+	
+	public static void main(String[] args) {
+		LeTestDeNoel cheshire = new LeTestDeNoel();
+		cheshire.dreamWistfully();
+	}
+}
